05c83b174cbe6b957fae38eb1c7ed40a55714cf7
Stanislav Sinyagin <ssinyagin@k-open.com>
2016-04-02 23:34:33 +0200

    installer improved

--- install_wireless_watchdog.sh
+++ install_wireless_watchdog.sh
@@ -2,11 +2,13 @@
  sed -e 's,.* dev \([a-zA-Z0-9]*\).*,DEV=\1,' \
  >/usr/local/etc/wireless_watchdog
 
-wget -O /usr/local/sbin/wireless_watchdog \
+wget -q --no-check-certificate -O /usr/local/sbin/wireless_watchdog \
 https://raw.githubusercontent.com/ssinyagin/wireless_watchdog/master/wireless_watchdog
 chmod u+x /usr/local/sbin/wireless_watchdog
 
 cat >/etc/cron.d/wireless_watchdog <<'EOT'
 */2 * * * * root /usr/local/sbin/wireless_watchdog
 EOT
+
+echo Installed wireless_watchdog
 
