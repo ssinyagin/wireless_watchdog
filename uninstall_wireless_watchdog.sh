80fd8f6518966cfefe58d1220950ad3dc8558113
Stanislav Sinyagin <ssinyagin@k-open.com>
2016-04-03 01:43:40 +0200

    uninstaller

--- uninstall_wireless_watchdog.sh
+++ uninstall_wireless_watchdog.sh
@@ -1,6 +1,8 @@
 rm -f /etc/cron.d/wireless_watchdog
 rm -f /usr/local/etc/wireless_watchdog
 rm -f /usr/local/sbin/wireless_watchdog
+rm -f /etc/logrotate.d/wireless_watchdog
+rm -f /var/log/wireless_watchdog*
 
 echo Uninstalled wireless_watchdog
 
